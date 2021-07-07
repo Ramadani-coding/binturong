386493141c94f20e3ea4db06e50780a988b28476
Ramadani-coding <ramacoding9@gmail.com>
2021-07-07 13:39:09 +0800

    first commit

---
+++ latihan.js
@@ -0,0 +1,11 @@
+alert("Selamat Datang!..");
+var lagi = true;
+
+while (lagi) {
+  var nama = prompt("Mengapa Binturonge belum bangun??");
+  alert(nama);
+
+  lagi = confirm("mau memberi alasan lagi??");
+}
+
+alert("terimakasih..");
